a36614c94b8b30174392b5d50c04b1f1e0578638
ILYA KRIVENOK <ilya@krivenok.com>
2023-07-09 16:41:33 +0300

    changed DEBUG settings and added IS_DEVELOPMENT to api/parsing_uzt_2/settings.py
    changed get_text_chunk function in __main__.py

--- api/parsing_uzt_2/settings.py
+++ api/parsing_uzt_2/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool('IS_DEVELOPMENT', default=True)
 ALLOWED_HOSTS = ['localhost', 'api']
 
 
